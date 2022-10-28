fcf0dc8d28aa5db254364033e6288a5a6e7643c6
ZryvNik <113814707+ZryvNik@users.noreply.github.com>
2022-10-28 15:11:28 +0400

    Убрал абстрактный класс Shape, добавил вместо него интерфейсы, которые будут описывать свойства фигур, добавил summary для всех методов (#1)
    Co-authored-by: Nikita Zarubin <zryvzarubinn@yandex.ru>

--- ShapeLibrary/Circle/Circle.cs
+++ ShapeLibrary/Circle/Circle.cs
@@ -2,20 +2,35 @@
 
 namespace ShapeLibrary
 {
-    public class Circle : Shape
+    /// <summary>
+    /// Фигура - круг
+    /// </summary>
+    public class Circle : IAreaAvailable
     {
         private readonly double _radius;
+
+        /// <exception cref="ArgumentException">Если радиус меньше нуля</exception>
         public Circle(double radius)
         {
             if (radius < 0)
                 throw new ArgumentException($"radius ({radius}) cannot be less then 0");
             _radius = radius;
         }
-        public override double Area()
-        {
-            return _radius * _radius * Math.PI / 2;
-        }
 
+        /// <summary>
+        /// Возвращает площадь круга 
+        /// </summary>
+        /// <returns>double</returns>
+        public double Area()
+        {
+            return _radius * _radius * Math.PI;
+        }
+        /// <summary>
+        /// Возвращает площадь круга по заданному радиусу
+        /// </summary>
+        /// <param name="radius"></param>
+        /// <exception cref="System.ArgumentException"></exception>
+        /// <returns>double</returns>
         public static double Area(double radius)
         {
             return new Circle(radius).Area();
---
+++ ShapeLibrary/Rightangle/ICanBeRightangle.cs
@@ -0,0 +1,14 @@
+﻿namespace ShapeLibrary
+{
+    /// <summary>
+    /// Интерфейс, описывающий фигуру, которая может быть прямоугольной
+    /// </summary>
+    public interface ICanBeRightangle
+    {
+        /// <summary>
+        /// Проверяет, является ли фигура прямоугольной
+        /// </summary>
+        /// <returns>True/False</returns>
+        bool IsRightangle();
+    }
+}
